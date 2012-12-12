ac3fc84030f332d09f2cf616b6b7aec2a2dcfc9e
Dmitry Konishchev <konishchev@gmail.com>
2012-12-12 17:51:41 +0400

    Added a copy-pasted prototype of setup.py

---
+++ setup.py
@@ -0,0 +1,55 @@
+# TODO
+"""pyvsb installation script."""
+
+from __future__ import unicode_literals
+
+from setuptools import find_packages, setup
+from setuptools.command.test import test as Test
+
+
+class PyTest(Test):
+    def finalize_options(self):
+        Test.finalize_options(self)
+        self.test_args = [ "tests" ]
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        pytest.main(self.test_args)
+
+
+description = """TODO"""
+
+if __name__ == "__main__":
+    setup(
+        name = "pyvsb",
+        version = "0.3",
+
+        description = "Very simple but powerful backup tool",
+        long_description = description,
+        url = "http://konishchevdmitry.github.com/psh/",
+
+        license = "GPL3",
+        author = "Dmitry Konishchev",
+        author_email = "konishchev@gmail.com",
+
+        classifiers = [
+            "Environment :: Console",
+            "Intended Audience :: End Users/Desktop",
+            "Intended Audience :: System Administrators",
+            "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+            "Natural Language :: English",
+            "Operating System :: MacOS :: MacOS X",
+            "Operating System :: POSIX",
+            "Operating System :: Unix",
+            "Programming Language :: Python :: 3",
+            "Topic :: System :: Archiving :: Backup",
+            "Topic :: Utilities",
+        ],
+        platforms = [ "unix", "linux", "osx" ],
+
+        packages = find_packages(),
+
+        cmdclass = { "test": PyTest },
+        tests_require = [ "pytest" ],
+    )
