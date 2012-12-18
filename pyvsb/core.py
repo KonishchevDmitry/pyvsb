1e97142b8ebfc5e1c5374ef385337f18d059e5ef
Dmitry Konishchev <konishchev@gmail.com>
2012-12-18 18:06:41 +0400

    Closed a TODO

--- pyvsb/core.py
+++ pyvsb/core.py
@@ -1,12 +1,15 @@
-# TODO
+"""Core tools."""
 
 class Error(Exception):
-    """A base class for all exceptions the module throws."""
+    """A base class for all exceptions the program raises."""
 
     def __init__(self, error, *args, **kwargs):
         super(Error, self).__init__(
             error.format(*args, **kwargs) if args or kwargs else error)
 
-class LogicalError(Exception):
+
+class LogicalError(Error):
+    """Exception for a logical error."""
+
     def __init__(self):
         super().__init__("Logical error")
